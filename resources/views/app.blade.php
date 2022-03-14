072b2bfa7634a9c775c9e31f19969fa46585d065
Richard Haddad <richard@haddads.net.au>
2022-03-14 23:53:36 +1000

    Set up React

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @yield('title')
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
+    <title>Document</title>
+</head>
+<body>
+    <div id="root"></div>
+    <script src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html>
