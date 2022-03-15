60a69627c114ce6f3963b7228ed1bda8d13a78e8
Richard Haddad <richard@haddads.net.au>
2022-03-15 21:17:06 +1000

    Fixed Mix CSS reference

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,12 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link rel="stylesheet" href="/css/app.css">
 
     <title>Document</title>
 </head>
 <body>
     <div id="root"></div>
-    <script src="{{ mix('/js/app.js') }}"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>
